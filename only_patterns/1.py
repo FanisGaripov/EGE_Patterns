7effbb99a86ab09e38945b1dc42f8543513e2560
Fanis Garipov <151039847+FanisGaripov@users.noreply.github.com>
2026-01-29 22:55:54 +0300

    Create 1.py

---
+++ only_patterns/1.py
@@ -0,0 +1,9 @@
+from itertools import *
+
+s = '258 17 56 68 138 347 26 145'.split()
+v = 'AC CB BH HD DA AG CG GE EF FH'.split()
+print(*range(1, 9))
+for p in permutations('ABCDEFGH'):
+    if all(str(p.index(a) + 1) in s[p.index(b)] for a, b in v):
+        print(*p)
+        break
